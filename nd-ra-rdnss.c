5a101c0b6d385a4ac6a3dd3130f28c6a5c7b04e8
4hiziri <meirvg@gmail.com>
2017-10-19 15:03:21 +0900

    Arrangement

--- nd-ra-rdnss.c
+++ nd-ra-rdnss.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <libnet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
-// macro for icmpv6 flags
 #define ND_RA_MANAGED_CONFIG_FLAG 0x0800000
 #define ND_RA_OTHER_CONFIG_FLAG   0x0400000
 #define ND_RA_HOP_LIMIT           0x1000000
@@ -24,7 +18,7 @@ void build_icmpv6_rdnss_opt(libnet_t* l,
 int main(int argc, char** argv){
   if (argc != 5) {
     fprintf(stderr, "%s <interface> <src addr> <dist addr> <dns addr>\n", argv[0]);
-    return 1;
+    exit(1);
   }
 
   // set argv
@@ -32,10 +26,9 @@ int main(int argc, char** argv){
   char *dist_addr = argv[2];
   char *src_addr = argv[3];
   char *dns_addr = argv[4];
-  
+
   libnet_t *l;
-  int id, seq;
-  libnet_in6_addr sip, dip, trg, dns;
+  libnet_in6_addr sip, dip, trg;
   char errbuf[LIBNET_ERRBUF_SIZE];
 
   /***************************************************************
@@ -45,24 +38,19 @@ int main(int argc, char** argv){
   if(l == NULL) {
     printf("libnet_init: %s\n", errbuf);
     exit(1);
-  }  
-
-  // get rand val for session.
-  libnet_seed_prand(l);
-  id = libnet_get_prand(LIBNET_PRu32);
-  seq = libnet_get_prand(LIBNET_PRu32);
+  }
 
   // get ipv6-addr struct
-  sip = libnet_name2addr6(l, src_addr, LIBNET_DONT_RESOLVE);   
+  sip = libnet_name2addr6(l, src_addr, LIBNET_DONT_RESOLVE);
   dip = libnet_name2addr6(l, dist_addr, LIBNET_DONT_RESOLVE);
 
-  /********************************* 
+  /*********************************
    *   build router advertisement  *
-   *********************************/  
+   *********************************/
   uint32_t lt = LIFETIME_INF;
   uint8_t payload[16];
   build_icmpv6_rdnss_opt(l, &trg, payload, lt, dns_addr);
-  
+
   libnet_build_icmpv6_ndp_nadv(
 			       ND_ROUTER_ADVERT,                               // uint8_t type
 			       0,                                              // uint8_t code
@@ -74,7 +62,7 @@ int main(int argc, char** argv){
 			       l,                                              // libnet_t* context
 			       0                                               // libnet_ptag_t ptag, 0 means create new one
 			       );
-  
+
   // build ipv6 packet
   libnet_build_ipv6(
 		    0,                                        // uint8_t traffic class
@@ -89,14 +77,14 @@ int main(int argc, char** argv){
 		    l,                                        //libnet_t* l
 		    0                                         //libnet_ptag_t ptag
 		    );
-    
+
   if(libnet_write(l) == -1) {
     printf("libnet_write: %s\n", libnet_geterror(l));
     exit(1);
   }
 
   libnet_destroy(l);
- 
+
   return 0;
 }
 
@@ -116,10 +104,10 @@ void build_icmpv6_rdnss_opt(libnet_t* l,
   // copy address, builder funciton accepts only uint8_t*
   for (int i = 0; i < 16; i++)
     payload[i] = libnet_name2addr6(l, dns_addr, LIBNET_DONT_RESOLVE).__u6_addr.__u6_addr8[i];
-  
+
   header->__u6_addr.__u6_addr8[8] = 0x19; // type num RDNSS
   header->__u6_addr.__u6_addr8[9] = 0x2 + 0x1; // 0x2 + number_of_dns_addr
   header->__u6_addr.__u6_addr32[3] = lifetime;
-  
+
   return;
 }
