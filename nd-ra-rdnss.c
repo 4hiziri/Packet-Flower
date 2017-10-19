33aa329fc0adf897552ede32cc3ab3ccb340234f
4hiziri <meirvg@gmail.com>
2017-10-19 14:39:31 +0900

    extract as function

--- nd-ra-rdnss.c
+++ nd-ra-rdnss.c
@@ -11,78 +11,83 @@
 #define ND_RA_OTHER_CONFIG_FLAG   0x0400000
 #define ND_RA_HOP_LIMIT           0x1000000
 #define ND_OPT_RDNSS              0x19
+#define LIFETIME_INF              0xffffffff
+
+typedef struct libnet_in6_addr libnet_in6_addr;
+
+void build_icmpv6_rdnss_opt(libnet_t* l,
+			    libnet_in6_addr *header,
+			    uint8_t *payload,
+			    uint32_t lifetime,
+			    const char* dns_addr);
 
 int main(int argc, char** argv){
-  if (argc != 2) {
-    fprintf(stderr, "%s <interface>\n", argv[0]);
+  if (argc != 5) {
+    fprintf(stderr, "%s <interface> <src addr> <dist addr> <dns addr>\n", argv[0]);
     return 1;
   }
+
+  // set argv
+  char *interface = argv[1];
+  char *dist_addr = argv[2];
+  char *src_addr = argv[3];
+  char *dns_addr = argv[4];
   
-  char *interface = argv[1];  
   libnet_t *l;
-  libnet_ptag_t ptag;
   int id, seq;
-  struct libnet_in6_addr sip, dip, trg;
+  libnet_in6_addr sip, dip, trg, dns;
   char errbuf[LIBNET_ERRBUF_SIZE];
 
-  // initialize libnet, this must be called before other functions
+  /***************************************************************
+    initialize libnet, this must be called before other functionsn
+   ***************************************************************/
   l = libnet_init(LIBNET_RAW6, interface, errbuf);
   if(l == NULL) {
     printf("libnet_init: %s\n", errbuf);
     exit(1);
-  }
-  ptag = LIBNET_PTAG_INITIALIZER;
+  }  
 
+  // get rand val for session.
   libnet_seed_prand(l);
   id = libnet_get_prand(LIBNET_PRu32);
   seq = libnet_get_prand(LIBNET_PRu32);
 
-  char* host = "1::1";
-  sip = libnet_name2addr6(l, host, LIBNET_DONT_RESOLVE);   
-  dip = libnet_name2addr6(l, "ff02::ffff", LIBNET_DONT_RESOLVE);
+  // get ipv6-addr struct
+  sip = libnet_name2addr6(l, src_addr, LIBNET_DONT_RESOLVE);   
+  dip = libnet_name2addr6(l, dist_addr, LIBNET_DONT_RESOLVE);
 
   /********************************* 
    *   build router advertisement  *
-   *********************************/
-  
-  // TODO: extract as function
-  // Set RDNSS info here.
-  trg.__u6_addr.__u6_addr8[8] = 0x19; 
-  trg.__u6_addr.__u6_addr8[9] = 0x3;
-  trg.__u6_addr.__u6_addr8[12] = 0xff;
-  trg.__u6_addr.__u6_addr8[13] = 0xff;
-  trg.__u6_addr.__u6_addr8[14] = 0xff;
-  trg.__u6_addr.__u6_addr8[15] = 0xff;
-  uint8_t payload[] = {0xff, 0xff, 0xff, 0xff,
-		       0xff, 0xff, 0xff, 0xff,
-		       0xff, 0xff, 0xff, 0xff,
-		       0xff, 0xff, 0xff, 0xff};		      		       
+   *********************************/  
+  uint32_t lt = LIFETIME_INF;
+  uint8_t payload[16];
+  build_icmpv6_rdnss_opt(l, &trg, payload, lt, dns_addr);
   
   libnet_build_icmpv6_ndp_nadv(
-			       ND_ROUTER_ADVERT, // uint8_t type
-			       0, // uint8_t code
-			       0, // uint16_t check_sum
+			       ND_ROUTER_ADVERT,                               // uint8_t type
+			       0,                                              // uint8_t code
+			       0,                                              // uint16_t check_sum
 			       64 * ND_RA_HOP_LIMIT + ND_RA_OTHER_CONFIG_FLAG, // uint32_t flags
-			       trg, // libnet_in6_addr target
-			       payload, // payload, // uint8_t* payload
-			       16, // payload_len, // uint32_t payload size
-			       l, // libnet_t*
-			       0 // libnet_ptag_t ptag, 0->new
+			       trg,                                            // libnet_in6_addr target
+			       payload,                                        // uint8_t* payload
+			       16,                                             // uint32_t payload size
+			       l,                                              // libnet_t* context
+			       0                                               // libnet_ptag_t ptag, 0 means create new one
 			       );
   
   // build ipv6 packet
   libnet_build_ipv6(
-		    0, // uint8_t traffic class
-		    0, // uint32_t flow label
+		    0,                                        // uint8_t traffic class
+		    0,                                        // uint32_t flow label
 		    LIBNET_IPV6_H + LIBNET_ICMPV6_NDP_NADV_H, //uint16_t len
-		    IPPROTO_ICMP6,   //uint8_t nh -> next header
-		    64,              //uint8_t hl -> hop limit
-		    sip,             //libnet_in6_addr src
-		    dip,             //libnet_in6_addr dst
-		    NULL,            //uint8_t* payload
-		    0,               //uint32_t payload_s
-		    l,               //libnet_t* l
-		    0                //libnet_ptag_t ptag
+		    IPPROTO_ICMP6,                            //uint8_t nh -> next header
+		    64,                                       //uint8_t hl -> hop limit
+		    sip,                                      //libnet_in6_addr src
+		    dip,                                      //libnet_in6_addr dst
+		    NULL,                                     //uint8_t* payload
+		    0,                                        //uint32_t payload_s
+		    l,                                        //libnet_t* l
+		    0                                         //libnet_ptag_t ptag
 		    );
     
   if(libnet_write(l) == -1) {
@@ -93,4 +98,28 @@ int main(int argc, char** argv){
   libnet_destroy(l);
  
   return 0;
+}
+
+/**
+ * set config value to header and payload.
+ * @param l libnet context
+ * @param header header of RDNSS, set some value into this
+ * @param payload dns address is set here
+ * @param lifetime lifetime of dns server
+ * @param dns_addr address of dns server, like "2001:db8::1"
+ */
+void build_icmpv6_rdnss_opt(libnet_t* l,
+			    libnet_in6_addr *header,
+			    uint8_t *payload,
+			    uint32_t lifetime,
+			    const char* dns_addr){
+  // copy address, builder funciton accepts only uint8_t*
+  for (int i = 0; i < 16; i++)
+    payload[i] = libnet_name2addr6(l, dns_addr, LIBNET_DONT_RESOLVE).__u6_addr.__u6_addr8[i];
+  
+  header->__u6_addr.__u6_addr8[8] = 0x19; // type num RDNSS
+  header->__u6_addr.__u6_addr8[9] = 0x2 + 0x1; // 0x2 + number_of_dns_addr
+  header->__u6_addr.__u6_addr32[3] = lifetime;
+  
+  return;
 }
