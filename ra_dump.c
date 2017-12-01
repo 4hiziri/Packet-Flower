517077556eb7236206101cf9785084dff88c8ef8
tkgsy <meirvg@gmail.com>
2017-12-01 15:56:24 +0900

    Add print capturing packet's len

--- ra_dump.c
+++ ra_dump.c
@@ -11,7 +11,7 @@ int main(int argc, char** argv){
   if ( pcap_lookupnet(dev, &net, &mask, err_buf) == -1 ) {
     fprintf(stderr, "can't get mask error\n");
     net = 0;
-    mask = 0;
+    mask = 0;    
   }
 
   pcap_t *handle = pcap_open_live(dev, // device
@@ -21,17 +21,17 @@ int main(int argc, char** argv){
 				  err_buf // error buf
 				  );
   if(handle == NULL) {
-    fprintf(stderr, "Init error\n");
+    fprintf(stderr, "Init error: %s\n", err_buf);
     exit(1);
   }
 
   if ( pcap_compile(handle, // pcap_t
 		    &fp, // compiled_exp
-		    "icmpv6.type == 134",
+		    "",
 		    0, // optimized_p
 		    net // netmask
 		    ) == -1 ) {
-    fprintf(stderr, "can't compile\n");
+    fprintf(stderr, "can't compile\n%s\n", err_buf);
     exit(1);
   }
 
@@ -39,6 +39,14 @@ int main(int argc, char** argv){
     fprintf(stderr, "can't filter\n");
     exit(1);
   }
+
+  const u_char *packet;
+  struct pcap_pkthdr header;
   
+  packet = pcap_next(handle, &header);
+
+  printf("%d", header.len);
+
+  pcap_close(handle);
   return 0;
 }
