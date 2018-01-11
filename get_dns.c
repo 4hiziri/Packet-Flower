8aa47275cd9df8e632d9ac404d4783b749ba3937
tkgsy <meirvg@gmail.com>
2018-01-11 11:09:00 +0900

    Modified to print dns-addr

--- get_dns.c
+++ get_dns.c
@@ -1,18 +1,59 @@
-#include <netinet/in.h>
-#include <arpa/nameser.h>
-#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
 #include <NetworkManager.h>
 
-int main(int argc, char** argv){
-  struct hostent* host_info = gethostent();
+const char* const * get_ip4_nameservers(char* interface);
+const char* const * get_ip6_nameservers(char* interface);
 
-  // 2 is AF_INET
-  // 10 is AF_INET6
-  for(int i = 0; i < 3; i++){
-    printf("%d\n", host_info->h_addrtype);
-    printf("%x\n", host_info->h_addr_list[i].sin_addr.s_addr);
+int main(int argc, char** argv){
+  if (argc != 2) {
+    fprintf(stderr, "%s <interface>", argv[0]);
+    exit(1);
   }
-    
+
+  char* interface = argv[1];
+  
+  const char* const* ip4_dns = get_ip4_nameservers(interface);
+  const char* const* ip6_dns = get_ip6_nameservers(interface);
+
+  if (ip4_dns) {
+    for (int i = 0; ip4_dns[i]; i++)
+      g_print("nameserver[%d]: %s\n", i, ip4_dns[i]);
+  }
+
+  if (ip6_dns) {
+    for (int i = 0; ip6_dns[i]; i++)
+      g_print("nameserver[%d]: %s\n", i, ip6_dns[i]);
+  }
+
   return 0;
+}
+
+const char* const * get_ip4_nameservers(char *interface) {
+  NMClient *client;
+  NMDevice *device;
+  NMIPConfig *ipconfig;
+  const char* const* nameservers;
+
+  client = nm_client_new(NULL, NULL);
+  device = nm_client_get_device_by_iface(client, interface);  
+  ipconfig = nm_device_get_ip4_config(device);
+  nameservers = nm_ip_config_get_nameservers(ipconfig);
+
+  return nameservers;
+}
+
+const char* const * get_ip6_nameservers(char *interface) {
+  NMClient *client;
+  NMDevice *device;
+  NMIPConfig *ipconfig;
+  const char* const* nameservers;
+
+  client = nm_client_new(NULL, NULL);
+  device = nm_client_get_device_by_iface(client, interface);
+  ipconfig = nm_device_get_ip6_config(device);
+  nameservers = nm_ip_config_get_nameservers(ipconfig);
+
+  return nameservers;
 }
