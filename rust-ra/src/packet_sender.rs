9c098da66a22446051701dcaf307bbd4c357f1fb
tkgsy <meirvg@gmail.com>
2018-03-28 11:43:11 +0900

    Add comment

--- rust-ra/src/packet_sender.rs
+++ rust-ra/src/packet_sender.rs
@@ -1,6 +1,7 @@
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel;
 
+// TODO: set Config.channel_type = Layer3
 pub fn get_connection(interface: &NetworkInterface) -> Channel {
     match datalink::channel(&interface, Default::default()) {
         Ok(ether) => ether,
